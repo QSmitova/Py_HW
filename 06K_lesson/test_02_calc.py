a1de1db184c4301afd7df3156dcea2075e1c4350
qsmitova <qsmitova@gmail.com>
2024-11-23 23:38:05 +0400

    дз "Контрольная работа"

--- 06K_lesson/test_02_calc.py
+++ 06K_lesson/test_02_calc.py
@@ -10,22 +10,20 @@ def test_calc():
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
 
-    try:
-        delay_input = driver.find_element(By.ID, "delay")
-        delay_input.clear()
-        delay_input.send_keys("45")
+    delay_input = driver.find_element(By.ID, "delay")
+    delay_input.clear()
+    delay_input.send_keys("45")
 
-        driver.find_element(By.XPATH, "//span[text()='7']").click()
-        driver.find_element(By.XPATH, "//span[text()='+']").click()
-        driver.find_element(By.XPATH, "//span[text()='8']").click()
-        driver.find_element(By.XPATH, "//span[text()='=']").click()
+    driver.find_element(By.XPATH, "//span[text()='7']").click()
+    driver.find_element(By.XPATH, "//span[text()='+']").click()
+    driver.find_element(By.XPATH, "//span[text()='8']").click()
+    driver.find_element(By.XPATH, "//span[text()='=']").click()
 
-        wait = WebDriverWait(driver, 50)
-        result = wait.until(
-            EC.text_to_be_present_in_element((By.CLASS_NAME, "screen"), "15")
-        )
+    wait = WebDriverWait(driver, 50)
+    result = wait.until(
+        EC.text_to_be_present_in_element((By.CLASS_NAME, "screen"), "15")
+    )
 
-        assert result, "Результат равен 15"
+    assert result, "Результат равен 15"
 
-    finally:
-        driver.quit()
+    driver.quit()
--- 06K_lesson/test_03_shop.py
+++ 06K_lesson/test_03_shop.py
@@ -8,29 +8,27 @@ def test_shop():
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     driver.get("https://www.saucedemo.com/")
 
-    try:
-        driver.find_element(By.ID, "user-name").send_keys("standard_user")
-        driver.find_element(By.ID, "password").send_keys("secret_sauce")
-        driver.find_element(By.ID, "login-button").click()
+    driver.find_element(By.ID, "user-name").send_keys("standard_user")
+    driver.find_element(By.ID, "password").send_keys("secret_sauce")
+    driver.find_element(By.ID, "login-button").click()
 
-        driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
-        driver.find_element(By.ID, "add-to-cart-sauce-labs-bolt-t-shirt").click()
-        driver.find_element(By.ID, "add-to-cart-sauce-labs-onesie").click()
+    driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
+    driver.find_element(By.ID, "add-to-cart-sauce-labs-bolt-t-shirt").click()
+    driver.find_element(By.ID, "add-to-cart-sauce-labs-onesie").click()
 
-        driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
+    driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
 
-        driver.find_element(By.ID, "checkout").click()
+    driver.find_element(By.ID, "checkout").click()
 
-        driver.find_element(By.ID, "first-name").send_keys("Tatiana")
-        driver.find_element(By.ID, "last-name").send_keys("Fedoseeva")
-        driver.find_element(By.ID, "postal-code").send_keys("443100")
+    driver.find_element(By.ID, "first-name").send_keys("Tatiana")
+    driver.find_element(By.ID, "last-name").send_keys("Fedoseeva")
+    driver.find_element(By.ID, "postal-code").send_keys("443100")
 
-        driver.find_element(By.ID, "continue").click()
+    driver.find_element(By.ID, "continue").click()
 
-        total_element = driver.find_element(By.CLASS_NAME, "summary_total_label")
-        total_value = total_element.text
+    total_element = driver.find_element(By.CLASS_NAME, "summary_total_label")
+    total_value = total_element.text
 
-        assert total_value == "Total: $58.29"
+    assert total_value == "Total: $58.29"
 
-    finally:
-        driver.quit()
+    driver.quit()
